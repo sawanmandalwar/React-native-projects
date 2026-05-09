437955541adccf494aa6c93b729f282b716e2709
Sawan Mandalwar <sawanmandalwar@gmail.com>
2026-05-09 14:51:33 +0530

    Updated index.js features

--- appwriteauth11/index.js
+++ appwriteauth11/index.js
@@ -2,8 +2,27 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './src/App';
-import {name as appName} from './app.json';
+import { AppRegistry, LogBox, Platform, StatusBar } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
 
+// Ignore warning logs in development
+if (__DEV__) {
+
+  LogBox.ignoreAllLogs();
+  
+}
+
+// Customize Status Bar
+StatusBar.setBarStyle('light-content');
+StatusBar.setBackgroundColor('#6200ee');
+
+// Platform check
+if (Platform.OS === 'android') {
+  console.log('Running on Android');
+} else {
+  console.log('Running on iOS');
+}
+
+// Register Main App
 AppRegistry.registerComponent(appName, () => App);
